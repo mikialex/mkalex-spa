b7791a6062410879496ab61f756704219b9b0752
MikiAlex <18516340862@163.com>
2017-06-27 16:05:31 +0800

    change router structure

--- mkalex-pager/src/router/index.js
+++ mkalex-pager/src/router/index.js
@@ -1,32 +1,55 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 import Home from '@/pages/home'
-import Article from '@/pages/article'
+import Article from '@/pages/article/article'
+import ArticleStream from '@/pages/article/stream'
+import ArticleArchice from '@/pages/article/archive'
 import Portifolio from '@/pages/portfolio'
 import Collection from '@/pages/collection'
 import Meta from '@/pages/meta'
 
 import Caicai from '@/pages/caicai'
+import Copyright from '@/pages/copyright'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/hello',
-      name: 'Hello',
-      component: Hello
-    },
-   {
+  routes: [{
       path: '/',
       name: 'home',
       component: Home
     },
-     {
+    {
       path: '/article',
       name: 'article',
-      component: Article
-    }
+      component: Article,
+      children: [
+        {
+          path: 'stream',
+          component: ArticleStream
+        },
+        {
+          path: 'archive',
+          component: ArticleArchice
+        }
+      ]
+    },
+    {
+      path: '/portfolio',
+      name: 'portifolio',
+      component: Portifolio
+    },{
+      path: '/collection',
+      name: 'collection',
+      component: Collection
+    },{
+      path: '/meta',
+      name: 'meta',
+      component: Meta
+    },{
+      path: '/caicai',
+      name: 'caicai',
+      component: Caicai
+    },
   ]
 })
