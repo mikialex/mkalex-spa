b86d4b4b429ab8b24d87b1fa58dd65694660d6ee
MikiAlex <18516340862@163.com>
2018-10-14 20:44:50 +0800

    animation improve

--- mkalex-site/src/components/homepage/introduction.vue
+++ mkalex-site/src/components/homepage/introduction.vue
@@ -1,39 +1,97 @@
 <template>
   <container-mid>
-    <div class="intro">
+
+    <div class="intro" >
       <div class="sep">
-        <img src="../../assets/avatar-back.png" alt="">
-        <p>Here i write things about technology, design, and my life. this site
-          is also as my personal portfolio, my project archive. 
-          any content should shared, distributed
-          with my explicit permission.
-        
-        </p>
+
+        <transition name="upshow">
+          <img src="../../assets/avatar-back.png" class="size" v-if="show1">
+        </transition>
+        <div v-if="!show1" class="size"></div>
+
+        <transition name="upshow">
+          <p v-if="show2" class="size"
+          >Here i write things about technology, design, and my life. this site
+            is also as my personal portfolio, my project archive. 
+            any content should shared, distributed
+            with my explicit permission.
+          </p>
+        </transition>
+        <div v-if="!show2" class="size"></div>
       </div>
       <div class="sep">
-        <p>
-          I am interested in GUI system, 
-          computer graphics, CAD, simulation, visulization, procedural aesthetics. 
-          With the background of art and design.
-          I believe the greatest thing came from fusion of art and technology.
-        </p>
-        <p>
-          Best wishes for every visitor, may you find some useful reference, enthusiasm
-          about magic fields, and the beauty and happyness of plain daily life.
-        </p>
+
+        <transition name="upshow">
+          <p v-if="show3" class="size">
+            I am interested in GUI system, 
+            computer graphics, CAD, simulation, visulization, procedural aesthetics. 
+            With the background of art and design.
+            I believe the greatest thing came from fusion of art and technology.
+          </p>
+        </transition>
+        <div v-if="!show3" class="size"></div>
+
+        <transition name="upshow">
+          <p  v-if="show4" class="size">
+            Best wishes for every visitor, may you find some useful reference, enthusiasm
+            about magic fields, and the beauty and happyness of plain daily life.
+          </p>
+        </transition>
+
+        <div v-if="!show4" class="size"></div>
       </div>
     </div>
+
   </container-mid>
 </template>
 
+<script>
+async function wait(time){
+  return new Promise((resolve)=>{
+    setTimeout(()=>{
+      resolve();
+    }, time);
+  })
+}
+
+export default {
+  data(){
+    return {
+      show1: false,
+      show2: false,
+      show3: false,
+      show4: false,
+    }
+  },
+  async mounted(){
+    this.show1=true;
+    await wait(100);
+    this.show2=true;
+    await wait(100);
+    this.show3=true;
+    await wait(100);
+    this.show4=true;
+  }
+}
+</script>
+
+
 <style lang="scss" scoped>
+.upshow-enter-active,  {
+  transition: all 0.4s ease-out;
+}
+.upshow-enter, .upshow-leave-active {
+  opacity: 0;
+  transform: translateY(30px);
+}
+
 .intro{
   margin-top:100px;
   display: flex;
   align-items: center;
   justify-content: space-between;
   width:100%;  
-  @media(max-width:900px){
+  @media(max-width:1000px){
     flex-direction: column;
     >img{
       align-self: center;
@@ -52,29 +110,31 @@
   color: #333;
   @media(max-width:500px){
     flex-direction: column;
-    >img{
+    img{
       align-self: center;
     }
   }
-  >img{
-    width:220px;
-    height:220px;
-    min-width:220px;
-    min-height:220px;
+  img{
     border-radius: 3px;
+    margin-bottom:30px;
   }
-  >p{
+  p{
     display: flex;
     align-items: center;
+    margin:0px;
+    margin-bottom:30px;
+    padding: 10px;
+    box-sizing: border-box;
+    border: 1px solid #eee;
+    border-radius: 3px;
+  }
+}
+
+.size{
     width:220px;
     height:220px;
     min-width:220px;
     min-height:220px;
-    margin:0px;
-    padding: 10px;
-    box-sizing: border-box;
-    // border: 1px solid #888;
-  }
 }
 </style>
 
--- mkalex-site/src/transitions/splice-page.vue
+++ mkalex-site/src/transitions/splice-page.vue
@@ -19,12 +19,12 @@ export default {
 }
 .splice-enter, .splice-leave-active {
   opacity: 0;
-  // transform: translate(100px);
-//   transform-origin:50% 50vh;
-//   transform: rotate3d(0, 1, 0, 40deg) 
-//   rotate3d(1, 0, 0, 25deg)
-//   rotate3d(2, 1, 3, -10deg)
-//   translate(100px);
+  transform: translate(10px);
+  transform-origin:0% 0%;
+  // transform: rotate3d(0, 1, 0, 40deg) 
+  // rotate3d(1, 0, 0, 25deg)
+  // rotate3d(2, 1, 3, -10deg)
+  // translate(100px);
 }
 
 </style>
